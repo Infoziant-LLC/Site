926e9d2141b4eaa4a25ea68cabef4b8d55e325da
Sundar Karthik S <sundar1007karthik@gmail.com>
2024-10-04 14:02:32 +0530

    done

--- assets/js/contact-form.js
+++ assets/js/contact-form.js
@@ -1,51 +1,67 @@
 jQuery(document).ready(function () {
-	jQuery('#submit').on('click',document,function(){
-			if(jQuery('#captcha_val').val()!=jQuery('#captcha_text').val()){
-				$('#captcha_text').parent('div').append('<span class="error">Captch is not match</span>');
-			}
-			else{
-				jQuery("#contactpage").validate({
-					submitHandler : function (e) {
-						submitSignupFormNow(jQuery("#contactpage"))
-					},
-					rules : {
-						fname : {
-							required : true
-						},
-						phone : {
-							required : true,
-							number : true
-						},
-						email : {
-							required : true,
-							email : true
-						}
-					},
-					errorElement : "span",
-					errorPlacement : function (e, t) {
-						e.appendTo(t.parent())
-					}
-				});
-				// submitSignupFormNow = function (e) {
-				// 	var t = e.serialize();
-				// 	var n = "contact-form.php";
-				// 	jQuery.ajax({
-				// 		url : n,
-				// 		type : "POST",
-				// 		data : t,
-				// 		success : function (e) {
-				// 			var t = jQuery.parseJSON(e);
-				// 			if (t.status == "Success") {
-				// 				jQuery("#form_result").html('<span class="form-success alert alert-success d-block">' + t.msg + "</span>");
-				// 			} else {
-				// 				jQuery("#form_result").html('<span class="form-error alert alert-danger d-block">' + t.msg + "</span>")
-				// 			}
-				// 			jQuery("#form_result").show();
-				// 		}
-				// 	});
-				// 	return false
-				// }
-		}
-	});
-	
-})
+    jQuery('#submit').on('click', function (event) {
+        event.preventDefault(); // Prevent default form submission
+
+        if (jQuery('#captcha_val').val() != jQuery('#captcha_text').val()) {
+            $('#captcha_text').parent('div').append('<span class="error">Captcha does not match</span>');
+        } else {
+            jQuery("#contactpage").validate({
+                submitHandler: function (form) {
+                    submitSignupFormNow(form);
+                },
+                rules: {
+                    fname: {
+                        required: true
+                    },
+                    phone: {
+                        required: true,
+                        number: true
+                    },
+                    email: {
+                        required: true,
+                        email: true
+                    }
+                },
+                errorElement: "span",
+                errorPlacement: function (error, element) {
+                    error.appendTo(element.parent());
+                }
+            });
+        }
+    });
+
+    function submitSignupFormNow(form) {
+        var formData = jQuery(form).serialize();
+
+        jQuery.ajax({
+            url: 'contact-form.php', // Ensure this is the correct URL
+            type: 'POST',
+            data: formData,
+            success: function (response) {
+                var result = jQuery.parseJSON(response);
+                if (result.status === "Success") {
+                    showSuccessMessage();
+                } else {
+                    showErrorMessage();
+                }
+            },
+            error: function () {
+                showErrorMessage();
+            }
+        });
+    }
+
+    function showSuccessMessage() {
+        // Hide the form
+        $('#contactpage').hide();
+        // Display success message
+        $('#form-message').html('<h2>Thank you! We will contact you soon.</h2>').css('color', 'green').show();
+    }
+
+    function showErrorMessage() {
+        // Clear the form fields
+        $('#contactpage')[0].reset();
+        // Display error message
+        $('#form-message').text('Form submission failed. Please try again.').css('color', 'red').show();
+    }
+});
